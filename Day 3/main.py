80dca6e658c64c6e17303c3e64d89a43ed5d9683
Ben Joseph <ben.joseph81@gmail.com>
2024-12-13 16:04:13 -0600

    Day 3 Part 1

---
+++ Day 3/main.py
@@ -0,0 +1,24 @@
+import re
+find_all_examples = []
+total = 0
+
+with open('input.txt', 'r') as file:
+    lines = [line.rstrip('\n') for line in file.readlines()]
+
+master_list = []
+
+for string in lines:
+    master_list.append(string)
+
+for string in master_list:
+    find_all_examples = re.findall('mul\(\d{1,3},\d{1,3}\)', string)
+
+for example in find_all_examples:
+    numbers = re.findall('\d{1,3},\d{1,3}', example)
+
+    num_1 = numbers[0]
+    num_2 = numbers[1]
+    value = num_1 * num_2
+    total += value
+
+print(total)
